4f01b0a21bb86e4c4387d3961f19c8b5182260f6
Mercy Crown <140126752+mercycrownadet@users.noreply.github.com>
2026-09-24 15:43:24 +0100

    Create apply_discount_function.py

---
+++ apply_discount_function.py
@@ -0,0 +1,15 @@
+def apply_discount(price, discount):
+    # Just check for numbers, ignore the sneaky boolean edge-case
+    if not isinstance(price, (int, float)):
+        return "The price should be a number"
+    
+    if not isinstance(discount, (int, float)):
+        return "The discount should be a number"
+    
+    if price <= 0:
+        return "The price should be greater than 0"
+    
+    if discount < 0 or discount > 100:
+        return "The discount should be between 0 and 100"
+    
+    return price - (price * (discount / 100))
